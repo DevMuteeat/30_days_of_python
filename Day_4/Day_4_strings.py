3f8e1331d3452caa1a55d1aec06301da1f6c4e5f
DevMuteeat <muteeatolaide@gmail.com>
2024-08-07 02:18:54 +0100

    Asabeneh

---
+++ Day_4/Day_4_strings.py
@@ -0,0 +1,33 @@
+# number 1
+string_1 = 'Thirty', 'Days', 'Of', 'Python'
+print(' '.join(string_1))
+
+# number 3
+company = "Coding For All"
+
+# number4
+print (company)
+
+# number5
+print(len(company))
+
+# number6
+print(company.upper())
+
+# number 7
+print(company.lower())
+
+# number8
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+# number11
+print(company.replace('Coding', 'Python'))
+
+# number13
+print(company.split())
+
+# number33
+sentence = "I am enjoying this challenge.\nI wonder what is next"
+print(sentence)
